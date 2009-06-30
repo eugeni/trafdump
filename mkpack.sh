57d50488a44c856ae75b6b97ee9b199a9bff833d
Eugeni Dodonov <eugeni@mandriva.com>
2009-06-30 07:32:58 -0300

    Updated mkpack to support git repo.

--- mkpack.sh
+++ mkpack.sh
@@ -3,7 +3,8 @@
 # Creates a binary pack for current version.
 #
 
-REVNO=$(bzr revno)
+#REVNO=$(bzr revno)
+REVNO=$(git log --pretty=oneline|wc -l)
 
 COMPRESS="-t7z -m0=BCJ2 -m1=LZMA:d=25:fb=255:a=2:lc=7:mf=bt3 -m2=LZMA:d=19:fb=128:a=2:lc=0:lp=2:mf=bt3 -m3=LZMA:d=19:fb=128:a=2:lc=0:lp=2:mf=bt3 -mb0:1 -mb0s1:2 -mb0s2:3"
 
